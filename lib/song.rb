4ecf214eda01916e549660275253b0d6384bf360
John <jyk595@gmail.com>
2021-08-10 20:59:38 -0700

    commit tree eed2aacbfe87f88be9932e82d02f8eae15106275

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  @@song_num = 0
+  
+  attr_accessor :name :artist :genre
+
+  def initialize (name, artist, genre)
+    @name = names
+    @artist = artists
+    @genre = genre
+  end
+end
